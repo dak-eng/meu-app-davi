94312e33a8f984b3602f2d19be93b1eea5cedc07
Davi Macedo Guerra <ben0067248@aluno.firjansenaisesi.com.br>
2026-06-18 21:47:08 -0300

    aaaaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,38 @@
-import { useState } from 'react'
-import heroImg from './assets/minecraft.png'
-import './App.css'
+import'./App.css'
 
-function PrimeiroComponente() {
+function BotaoSenai() {
+const clicarbotao = () => {
+  alert("Este é o Botão")
+}
 
-const nome = document.getElementsByName("nome");
-   const exibirAlerta = () => {
-    alert(`Você digitou: ${nome[0].value}`);
-   }
-  
   return (
-    
-  <div id="social">
-    <h1>Qual seu nome?</h1>
-    <input type="text" name="nome" placeholder='Digite seu nome...' />
-<br />
-    <input 
-      type="button" 
-      value="Mostrar" 
-      onClick={exibirAlerta} 
-    />
-  
-</div>
-  )
+  <button
+    style={{
+    background:"#fb83ff",
+    color: "#000000",
+    padding: "15px 25px",
+    fontWeight: "bold",
+    border: "none",
+  }}
+  onClick = {clicarbotao}
+  >
+
+Clique aqui!
+</button>
+  );
 }
 
 function App() {
-  const [count, setCount] = useState(67)
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-        </div>
-        <div>
-          <h1>casinobiribinha</h1>
-          <p>
-            casino vai aqui - <code>src/App.jsx</code> e biribinha aqui - <code>HMR</code>
-          </p>
-          <p>biluteteia gosta de codar em react
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      <PrimeiroComponente/>
-      
-      <br />
-        
-      </section>
-      
-      <div className="ticks"></div>
-    </>
+<>
+  <div>
+  
+    <h1>Esse é meu botao senai!</h1>
+    <BotaoSenai/>
+    
+  </div>
+</>
   )
 }
 export default App
