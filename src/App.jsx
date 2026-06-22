0a5e52cc1ae6d7d471b12d7b95870720cb0dcced
Davi Macedo Guerra <ben0067248@aluno.firjansenaisesi.com.br>
2026-06-22 19:13:49 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import minecraft from './assets/minecraft.png'
 import'./App.css'
 
 function BotaoSenai() {
@@ -8,11 +9,13 @@ const clicarbotao = () => {
   return (
   <button
     style={{
-    background:"#fb83ff",
+    background:"linear-gradient(135deg, #d859ff, #fb83ff)",
     color: "#000000",
     padding: "15px 25px",
     fontWeight: "bold",
-    border: "none",
+    borderRadius:"12px",
+    border:"none",
+    boxShadow:"0 4px 15px rgba(255, 1, 242, 0.47)"
   }}
   onClick = {clicarbotao}
   >
@@ -28,11 +31,15 @@ function App() {
 <>
   <div>
   
-    <h1>Esse é meu botao senai!</h1>
+    <h1 className="font">Esse é meu botao senai!</h1>
     <BotaoSenai/>
-    
+   <br/><br/><br/>
+   <div>
+    <img src={minecraft} alt="minecraft" className="minecraft-img"></img>
+   </div>
   </div>
 </>
   )
 }
+
 export default App
