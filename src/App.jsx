eadcdb4f11f455fb41f399dec1c21a6d278a66ff
Davi Macedo Guerra <ben0067248@aluno.firjansenaisesi.com.br>
2026-06-22 21:40:58 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,149 @@
-import minecraft from './assets/minecraft.png'
-import'./App.css'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
 
-function BotaoSenai() {
-const clicarbotao = () => {
-  alert("Este é o Botão")
-}
-
+function Titulo() {
   return (
-  <button
-    style={{
-    background:"linear-gradient(135deg, #d859ff, #fb83ff)",
-    color: "#000000",
-    padding: "15px 25px",
-    fontWeight: "bold",
-    borderRadius:"12px",
-    border:"none",
-    boxShadow:"0 4px 15px rgba(255, 1, 242, 0.47)"
-  }}
-  onClick = {clicarbotao}
-  >
-
-Clique aqui!
-</button>
-  );
+         
+          <h1>Lets Get Started!</h1>
+       
+  )
+}
+function Hero() {
+  return ( 
+  <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+           <img src={viteLogo} className="vite" width="170" height="179" alt="" />
+            <img src={reactLogo} className="framework" width="170" height="179" alt="" />
+        </div>
+  )
+}
+function Botao() {
+ const [count, setCount] = useState(0)
+  
+  return (
+    <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+  )
+}
+function  Social() {
+  return (
+    <> <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+    </>
+  )
 }
 
 function App() {
 
   return (
-<>
-  <div>
-  
-    <h1 className="font">Esse é meu botao senai!</h1>
-    <BotaoSenai/>
-   <br/><br/><br/>
-   <div>
-    <img src={minecraft} alt="minecraft" className="minecraft-img"></img>
-   </div>
-  </div>
-</>
+    <>
+      <section id="center">
+        <Hero/>
+        <Titulo/>
+       <div>
+       <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        
+        <Botao/>
+      </section>
+      
+      <br />
+      
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+       <Social/>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
   )
 }
-
 export default App
