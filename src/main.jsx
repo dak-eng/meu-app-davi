90e8e7b4d9788e0085cddf9b9f233b1c448ff84d
Davi Macedo Guerra <ben0067248@aluno.firjansenaisesi.com.br>
2026-06-25 18:18:33 -0300

    pupu

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
 import './index.css'
-import App from './App';
 
-ReactDOM.createRoot(
+createRoot(
   document.getElementById('root')
 ).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
   <StrictMode>
-    <App />
-  </StrictMode>
+    <BrowserRouter>          
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
